c9c741c3e661ff4be360af819370556a0cab2d55
Jose Fernandez <jfernandez@pokeraffiliatesolutions.com>
2011-01-28 13:27:08 -0600

    Add the --no-check-certificate opt to wget, github's ssl cert confuses it

--- install.sh
+++ install.sh
@@ -8,13 +8,13 @@ fi
 clear
 echo; echo 'Installing DOS-Deflate 0.6'; echo
 echo; echo -n 'Downloading source files...'
-wget -q -O /usr/local/ddos/ddos.conf https://github.com/jfernandez/ddos-deflate/raw/master/ddos.conf
+wget -q -O /usr/local/ddos/ddos.conf https://github.com/jfernandez/ddos-deflate/raw/master/ddos.conf --no-check-certificate
 echo -n '.'
-wget -q -O /usr/local/ddos/LICENSE https://github.com/jfernandez/ddos-deflate/raw/master/LICENSE
+wget -q -O /usr/local/ddos/LICENSE https://github.com/jfernandez/ddos-deflate/raw/master/LICENSE --no-check-certificate
 echo -n '.'
-wget -q -O /usr/local/ddos/ignore.ip.list https://github.com/jfernandez/ddos-deflate/raw/master/ignore.ip.list
+wget -q -O /usr/local/ddos/ignore.ip.list https://github.com/jfernandez/ddos-deflate/raw/master/ignore.ip.list --no-check-certificate
 echo -n '.'
-wget -q -O /usr/local/ddos/ddos.sh https://github.com/jfernandez/ddos-deflate/raw/master/ddos.sh
+wget -q -O /usr/local/ddos/ddos.sh https://github.com/jfernandez/ddos-deflate/raw/master/ddos.sh --no-check-certificate
 chmod 0755 /usr/local/ddos/ddos.sh
 cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
 echo '...done'
