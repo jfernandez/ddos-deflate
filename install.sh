7273e9305fc21753ff640326771eb6dbc76a5b11
Jose Fernandez <jfernandez@pokeraffiliatesolutions.com>
2011-01-28 13:11:59 -0600

    Do not automatically install the cron during installation (allow config first)

--- install.sh
+++ install.sh
@@ -18,12 +18,9 @@ wget -q -O /usr/local/ddos/ddos.sh https://github.com/jfernandez/ddos-deflate/ra
 chmod 0755 /usr/local/ddos/ddos.sh
 cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
 echo '...done'
-
-echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
-/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
-echo '.....done'
 echo; echo 'Installation has completed.'
 echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Install cron script: usr/local/ddos/ddos.sh --cron'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 echo
 cat /usr/local/ddos/LICENSE | less
